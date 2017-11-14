336ebbe5a8d20620300d33dca70726bfd922b40a
ArelRojo <33665006+ArelRojo@users.noreply.github.com>
2017-11-14 17:43:13 +0100

    Add files via upload

---
+++ IIPMath.java
@@ -0,0 +1,58 @@
+package pract6;
+
+/**
+ * La clase IIPMath implementa algunas operaciones matematicas:
+ * IIPMath.sqrt(double) e IIPMath.log(double).
+ *
+ * @author (IIP-PRG-DSIC-ETSINF)
+ * @version (Curso 2017-18)
+ */
+public class IIPMath {
+    
+    // A COMPLETAR
+    public final static double LOG_2 = 
+        
+    /**
+     * Devuelve la raiz cuadrada de x >= 0, con error epsilon > 0. 
+     * A COMPLETAR COMENTARIO
+     */
+    public static double sqrt(double x, double epsilon) {
+        // A COMPLETAR
+    }            
+
+    /**
+     * Devuelve la raiz cuadrada de x >= 0 con error 1e-15. 
+     * A COMPLETAR COMENTARIO
+     */
+    public static double sqrt(double x) {    
+        // A COMPLETAR
+    }  
+                    
+    /* ******************************************************** */
+    /* ******************************************************** */
+    /* ******************************************************** */
+                
+    /**
+     * Devuelve log(z), 1/2 <= z < 1, con error epsilon > 0.
+     * A COMPLETAR COMENTARIO
+     */
+    public static double logBase(double z, double epsilon) {
+        // A COMPLETAR
+    }
+            
+    /**
+     * Devuelve log(x), x > 0, con error epsilon > 0.
+     * A COMPLETAR COMENTARIO
+     */
+    public static double log(double x, double epsilon) {
+        // A COMPLETAR                
+    }
+   
+    /**
+     * Devuelve log(x), x > 0, con error 1e-15.
+     * A COMPLETAR COMENTARIO
+     */
+    public static double log(double x) {    
+        // A COMPLETAR
+                        
+}
